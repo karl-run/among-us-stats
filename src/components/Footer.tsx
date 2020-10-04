9ea9237a8e1bb796cde045fc7940c0c6c816c943
Karl J. Overå <k@rl.run>
2020-10-04 14:26:54 +0200

    make more mobile friendly

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,14 +6,29 @@ import { Typography } from '@material-ui/core';
 import Tooltip from '@material-ui/core/Tooltip';
 import Button from '@material-ui/core/Button';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { statsSlice } from '../store/statsRedux';
 
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  clearButton: {
+    [theme.breakpoints.down('xs')]: {
+      marginTop: theme.spacing(8),
+    },
+  },
+}));
+
 function Footer(): JSX.Element {
+  const classes = useStyles();
   const dispatch = useDispatch();
 
   return (
-    <Box m={2} display="flex" justifyContent="space-between">
+    <Box m={2} display="flex" justifyContent="space-between" className={classes.wrapper}>
       <Box display="flex" justifyContent="center" alignItems="center" style={{ opacity: 0.7 }}>
         <Box mr={1} pt={0.5}>
           <InfoIcon />
@@ -24,6 +39,7 @@ function Footer(): JSX.Element {
       </Box>
       <Tooltip title="DANGER! No warning will pop up">
         <Button
+          className={classes.clearButton}
           endIcon={<DeleteForeverIcon />}
           onClick={() => {
             dispatch(statsSlice.actions.resetSession());
--- src/components/table/TableToolbar.tsx
+++ src/components/table/TableToolbar.tsx
@@ -1,21 +1,48 @@
 import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import Tooltip from '@material-ui/core/Tooltip';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/PlusOne';
+import GamepadIcon from '@material-ui/icons/Gamepad';
 import { useDispatch } from 'react-redux';
+import Hidden from '@material-ui/core/Hidden';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { statsSlice } from '../../store/statsRedux';
 
+const useStyles = makeStyles({
+  leftMost: {
+    flex: '1 1 100%',
+  },
+});
+
 function TableToolbar(): JSX.Element {
+  const classes = useStyles();
   const dispatch = useDispatch();
 
   return (
     <Toolbar>
-      <Typography variant="h5" style={{ flex: '1 1 100%' }}>
-        Among Us Stats Tracker
-      </Typography>
+      <Hidden smDown>
+        <Typography variant="h5" className={classes.leftMost}>
+          Among Us Stats Tracker
+        </Typography>
+      </Hidden>
+      <Hidden mdUp>
+        <Box className={classes.leftMost} />
+      </Hidden>
+      <Tooltip title="View scoreboard">
+        <Button
+          endIcon={<GamepadIcon />}
+          aria-label="view scoreboard"
+          onClick={() => {
+            dispatch(statsSlice.actions.newGame());
+          }}
+          style={{ minWidth: '150px' }}
+        >
+          Summary
+        </Button>
+      </Tooltip>
       <Tooltip title="New game">
         <Button
           endIcon={<AddIcon />}
