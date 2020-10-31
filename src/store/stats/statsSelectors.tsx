3ee460c80f6fb794cdfb8c87d4467608a17fdf4b
Karl J. Overå <k@rl.run>
2020-10-31 21:37:49 +0100

    quick fix bug where normalized players can be missing

--- src/store/stats/statsSelectors.tsx
+++ src/store/stats/statsSelectors.tsx
@@ -3,17 +3,27 @@ import { RootState } from '../redux';
 import { EnhancedPlayer, Player, Session, SessionPlayer, UUID } from './statsRedux';
 import { getPlayer } from './statsUtils';
 
-const createEnhancePlayer = (players: Record<UUID, Player>) => (sessionPlayer: SessionPlayer): EnhancedPlayer => ({
-  ...getPlayer(players, sessionPlayer.playerId),
-  ...sessionPlayer,
-});
+const createEnhancePlayer = (players: Record<UUID, Player>) => (
+  sessionPlayer: SessionPlayer,
+): EnhancedPlayer | null => {
+  const player = getPlayer(players, sessionPlayer.playerId);
+
+  if (player == null) {
+    return null;
+  }
+
+  return {
+    ...player,
+    ...sessionPlayer,
+  };
+};
 
 export const getSession = (state: RootState): Session<EnhancedPlayer> => {
   const enhancePlayer = createEnhancePlayer(state.stats.players);
 
   return {
     ...state.stats.session,
-    players: state.stats.session.players.map(enhancePlayer),
+    players: state.stats.session.players.map(enhancePlayer).filter((it): it is Player & SessionPlayer => it != null),
   };
 };
 
@@ -22,7 +32,7 @@ export const getPreviousSessions = (state: RootState): Session<EnhancedPlayer>[]
 
   return state.stats.previousSessions.map((session) => ({
     ...session,
-    players: session.players.map(enhancePlayer),
+    players: session.players.map(enhancePlayer).filter((it): it is Player & SessionPlayer => it != null),
   }));
 };
 
--- src/store/stats/statsUtils.ts
+++ src/store/stats/statsUtils.ts
@@ -1,11 +1,7 @@
 import { Game, Player, UUID } from './statsRedux';
 
 export function getPlayer(players: Record<UUID, Player>, playerId: UUID): Player {
-  const player: Player | undefined = players[playerId];
-
-  if (!player) throw Error(`No player with ID ${playerId} found`);
-
-  return player;
+  return players[playerId] ?? null;
 }
 
 export type WinnersPerGameTuple = [winners: string[], wonByImpostors: boolean];
